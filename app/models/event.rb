1882c50d454f3aff48a0251b01b3cf77e93d7585
terachan3700 <chapico01@gmail.com>
2021-05-15 17:44:58 +0900

    イベント編集と削除機能を追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,13 @@ class Event < ApplicationRecord
   validates :end_at, presence: true
   validate :start_at_should_be_before_end_at
 
+  belongs_to :owner, class_name: "User"
+
+  def created_by?(user)
+    return false unless user
+    owner_id == user.id
+  end
+
   private
 
   def start_at_should_be_before_end_at
